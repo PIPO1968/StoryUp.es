db3fe8801146080ca90bb7376e169d278df24d24
PIPO1968 <pipo1968@gmail.com>
2025-10-08 00:02:45 +0100

    fix: permitir host de Render en allowedHosts para preview

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,7 @@ export default defineConfig({
             '@': path.resolve(__dirname, 'src'),
         },
     },
+    preview: {
+        allowedHosts: ["storyup-es-0cs1.onrender.com"]
+    }
 });
