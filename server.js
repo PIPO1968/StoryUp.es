4ef3bdffdc590659d3e358a85a022142d7646721
PIPO1968 <pipo1968@gmail.com>
2025-10-08 01:08:32 +0100

    Fix Express v5: cambiar '*' por '/*' en ruta catch-all

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/api/auth', require('./api/auth.js'));
 
 // Para cualquier otra ruta, servir index.html (SPA)
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
