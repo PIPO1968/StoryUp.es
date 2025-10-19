08e7c0c0721261ea624c9bab8f68c934b65cf7e1
PIPO1968 <pipo1968@gmail.com>
2025-10-19 23:39:47 +0100

    fix: añade require('bcrypt') para evitar error en login y registro

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const Data = require('../models/data');
