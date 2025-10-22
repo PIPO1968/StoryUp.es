2e1d056e31c907429a56aa08b0be39b5de022deb
PIPO1968 <pipo1968@gmail.com>
2025-10-22 09:18:22 +0100

    Fix: añadir importación de InfoBlock en App.js para corregir error de compilación en Vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { InfoBlock } from './App';
 import Register from './Register';
 import Perfil from './Perfil';
 import CrearHistoria from './CrearHistoria';
